a8637368d2eb36e496f5cda5c00e46d9fc55d43c
niujj <17356781611@163.com>
2019-05-27 21:49:36 +0800

    remote to ng

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.less']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) {
+    
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
